9f8faae4563305bbdc1a5a656907455e4ebe9d9d
mauricio-alves <mauricioalvesnutri@gmail.com>
2022-11-10 11:29:27 -0300

    challenge done

--- Semana02/aula01/desafio.js
+++ Semana02/aula01/desafio.js
@@ -1,5 +1,6 @@
 let sortedNumbers = [2, 1, 2];
-let equalResults = [];
+let previousSorts = [];
+let correctResults = [];
 let counter = 0;
 let tries = 0;
 let random;
@@ -8,34 +9,39 @@ function sort() {
   for (let i = 0; i <= sortedNumbers.length - 1; i++) {
     random = parseInt(Math.random() * 3 + 1);
     sortedNumbers[i] = random;
-    console.log("Sorted Numbers:", sortedNumbers);
-    tries = tries + 1;
+    previousSorts[tries] = [...sortedNumbers];
+    console.log("Números Sorteados:", sortedNumbers);
+    console.log("Último Sorteio:", previousSorts);
+    tries++;
 
     if (
-      sortedNumbers[0] === sortedNumbers[1] &&
-      sortedNumbers[0] === sortedNumbers[2] &&
-      counter < 3
+      (sortedNumbers[0] === sortedNumbers[1] &&
+        sortedNumbers[0] === sortedNumbers[2] &&
+        counter < 3) ||
+      (previousSorts.length > 1 &&
+        previousSorts[previousSorts.length - 2].toString() ===
+          sortedNumbers.toString() &&
+        counter < 3)
     ) {
-      console.log(`Porta ${sortedNumbers[0]}: aberta!`);
-      equalResults[counter] = [...sortedNumbers];
-      counter = counter + 1;
-    } else {
-      console.log("Tente de novo!");
+      console.log(`Porta ${counter + 1}: aberta!`);
+      correctResults[counter] = [...sortedNumbers];
+      counter++;
     }
+    console.log("Tente de novo!");
   }
 }
 
 for (let i = 0; i < 30; i++) {
   if (tries > 11) {
     console.log("Fim de jogo, você perdeu!");
-    console.log("tries", tries);
+    console.log("Tentativas:", tries);
     return;
   }
 
   if (counter === 3) {
     console.log("Fim de jogo, você venceu!");
-    console.log("Equal Results:", equalResults);
-    console.log("tries", tries);
+    console.log("Resultados corretos:", correctResults);
+    console.log("Tentativas:", tries);
     return;
   }
 
