62682f07104f4b5dea5cce66abc7b9be498c6183
mauricio-alves <mauricioalvesnutri@gmail.com>
2023-01-11 15:52:11 -0300

    aulas js

---
+++ Semana11/aula01/aulaJS_HTML_docs/src/js/exJS_04.js
@@ -0,0 +1,40 @@
+console.log("testando");
+
+let t1,
+  t2,
+  st1,
+  st2,
+  t1Txt,
+  t2Txt,
+  st1Txt,
+  st2Txt,
+  btnConfTurno,
+  btnConfSetor,
+  temp;
+
+t1 = 8;
+t2 = 4;
+
+t1Txt = document.querySelector("#func01-turno");
+t2Txt = document.querySelector("#func02-turno");
+st1Txt = document.querySelector("#func01-setor");
+st2Txt = document.querySelector("#func02-setor");
+
+t1Txt.value = `${t1}hrs`;
+t2Txt.value = `${t2}hrs`;
+
+document.querySelector("#btnConfTurno").addEventListener("click", function () {
+  t1 = t1 + t2; // 12
+  t2 = t1 - t2; // 8
+  t1 = t1 - t2; // 4
+  t1Txt.value = `${t1}hrs`;
+  t2Txt.value = `${t2}hrs`;
+});
+
+st1Txt.value = "Atendimento";
+st2Txt.value = "Entrega";
+document.querySelector("#btnConfSetor").addEventListener("click", function () {
+  temp = st1Txt.value;
+  st1Txt.value = st2Txt.value;
+  st2Txt.value = temp;
+});
---
+++ Semana11/aula01/aulaJS_HTML_docs/src/js/falsy_truthy.js
@@ -0,0 +1,27 @@
+// FALSY
+if (false) {
+}
+if (null) {
+}
+if (undefined) {
+}
+if (0) {
+}
+if (NaN) {
+}
+if ("") {
+}
+
+// TRUTHY
+if (true) {
+}
+if ([]) {
+}
+if ({}) {
+}
+if ("mylet") {
+}
+if (56) {
+}
+if (new Date()) {
+}
